cd18869a36ed6602f5e2171f9c0571d01243b4be
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-01-18 18:58:43 +0100

    Ejemplo 2
    Comienzo

---
+++ Ejemplo2/Ejemplo2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Programa realizado por
+//Dia 18 de Enero, 2023
+Console.WindowHeight = 25; //Ancho de la ventana
+Console.WindowWidth = 60;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear();
+Console.Title = "Programa de medio de ventas de un vendedor";
+Console.Write("\n\n");
+//Declaracion de Variables
+String nomb, ape;
+Double v1, v2, v3, prom;
+//Entrada
+Console.Write("\tEmpresas ADOC....");
+Console.Write("\n");
