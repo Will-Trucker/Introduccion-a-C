28410403511d64d371458182298e9806354ec1c0
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-01-23 02:04:33 +0100

    Actulización de Guia 1 - Practica 2
    Uso de las funciones Math en C# para evitar asignarle el valor a PI

---
+++ Practica2/Practica2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    //Calcular el volumen de una esfera
+    class Program
+    {
+        static void Main(string[] args){
+            double radio, volumen;
+
+            Console.WriteLine("Ingrese el Radio");
+            radio = double.Parse(Console.ReadLine());
+
+            /*volumen = ((4 * pi) * (radio * radio * radio) / 3);*/
+            volumen = ((4 * Math.PI) * (Math.Pow(radio,3) / 3));
+
+            Console.WriteLine("El resultado es .... " + volumen);
+
+            Console.ReadKey();
+        }
+    }
+}
